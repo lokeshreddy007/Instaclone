e10446ed6095b427139a698059869601593c9212
lokeshreddy007 <lokeshreddy160@gmail.com>
2020-05-14 19:20:39 +0530

    add Data base connection

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 const express = require("express");
+const mongoose = require("mongoose");
+
+const { MONGOURI } = require("./keys");
 const app = express();
 const PORT = 5000;
+
+mongoose.connect(MONGOURI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+mongoose.connection.on("connected", () => {
+  console.log("connected database");
+});
+
+mongoose.connection.on("error", (err) => {
+  console.log("connecting", err);
+});
 
 app.get("/", (req, res) => {
   res.send("hello word");
@@ -9,3 +22,5 @@ app.get("/", (req, res) => {
 app.listen(PORT, () => {
   console.log("server is running", PORT);
 });
+
+// hello hAHmI5wFemnUmlqS
