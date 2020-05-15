b57d20bdaf395dfe07b59b90b27defccb051efed
lokeshreddy007 <lokeshreddy160@gmail.com>
2020-05-15 10:33:07 +0530

    add password hash

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
+const User = mongoose.model("User");
+const bcrypt = require("bcryptjs");
 
 router.get("/", (req, res) => {
   res.send("hello");
@@ -10,7 +13,31 @@ router.post("/signup", (req, res) => {
   if (!name || !email || !password) {
     res.status(422).json({ error: "enter all values" });
   }
-  res.json({ message: "Sent data Succesfully" });
+  //res.json({ message: "Sent data Succesfully" });
+  User.findOne({ email: email })
+    .then((savedUser) => {
+      if (savedUser) {
+        return res.status(422).json({ error: "This email all ready exist" });
+      }
+      bcrypt.hash(password, 12).then((hashedpassword) => {
+        const user = new User({
+          name,
+          email,
+          password: hashedpassword,
+        });
+        user
+          .save()
+          .then((user) => {
+            res.json({ message: "saved successfully" });
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 });
 
 module.exports = router;
